11967893b12f4bdb9ba6868d1c09a35f796840d5
Yogeshshrestha <yogeshshrestha845@gmail.com>
2022-05-29 09:26:46 +0545

    mobile web developement with pixel perfect. A sample of real world project

---
+++ markup/js/jquery.main.js
@@ -0,0 +1,24 @@
+(function() {
+	initBurgerMenu('.menu__opener', 'nav--active')
+	initBurgerMenu('.search__opener', 'search--active')
+})()
+
+
+function initBurgerMenu(selector, activeClass = 'active') {
+	var isOpen = false
+	var body = document.querySelector('body')
+
+	document.querySelectorAll(selector)
+	.forEach(function(menuLink) {
+		menuLink.addEventListener('click', function(event) {
+			event.preventDefault()
+
+			if(isOpen)
+				body.classList.remove(activeClass)
+			else
+				body.classList.add(activeClass)
+
+			isOpen = !isOpen
+		})
+	})
+}
